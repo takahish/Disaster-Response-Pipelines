a34482cacfbb1159bbb01715288348e3cfb243d4
Takahiro Ishikawa <takahish.nil@gmail.com>
2018-10-09 22:17:47 +0900

    feat: Added some parameters to to_sql methods
    Added some parameters to pandas.DataFrame.to_sql methods to store data
    into SQLite.
    
    - if_exists: How to behave if the table already exists.
    - chunksize: Rows will be written in batches of this size at a time.
    
    When it is not provided a chunksize, the full result of the query is
    put in a dataframe at once. Sometimes the SQLite driver or the system
    might have a limit to this, and if the size of the data is more than
    the limit, you will get an error.

--- workspace/data/process_data.py
+++ workspace/data/process_data.py
@@ -19,7 +19,7 @@ def load_data(messages_filepath, categories_filepath):
         messages = pd.read_csv(messages_filepath)
     else:
         messages = pd.read_csv(messages_filepath + '.gz', compression='gzip')
-    
+
     # load categories dataset
     if os.path.exists(categories_filepath):
         categories = pd.read_csv(categories_filepath)
@@ -28,7 +28,7 @@ def load_data(messages_filepath, categories_filepath):
 
     # merge datasets
     df = pd.merge(messages, categories, on='id', how='outer')
-    
+
     return df
 
 def clean_data(df):
@@ -42,49 +42,49 @@ def clean_data(df):
     """
     # create a dataframe of the 36 individual category columns
     categories = df['categories'].str.split(';', expand=True)
-    
+
     # select the first row of the categories dataframe
     row = categories[0:1]
-    
+
     # use this row to extract a list of new column names for categories.
-    # one way is to apply a lambda function that takes everything 
+    # one way is to apply a lambda function that takes everything
     # up to the second to last character of each string with slicing
     category_colnames = [(v.split('-'))[0] for v in row.values[0]]
-    
+
     # rename the columns of `categories`
     categories.columns = category_colnames
-    
+
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].str[-1]
-    
+
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
 
     # Convert all value into binary (0 or 1)
     categories = (categories > 0).astype(int)
-    
+
     # drop the original categories column from `df`
     df.drop('categories', axis=1, inplace=True)
-    
+
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df, categories], axis=1)
-    
+
     # drop duplicates
     df.drop_duplicates(inplace=True)
-    
+
     return df
 
 def save_data(df, database_filename, table_name='CleanedDataTable'):
     """Save data into database.
-    
+
     Args:
         df: pandas.DataFrame. It contains disaster messages and categories that are cleaned.
         database_filename: String. Dataframe is saved into this database file.
         table_name: String. Dataframe is saved into this table on database.
     """
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql(table_name, engine, index=False)
+    df.to_sql(table_name, engine, index=False, if_exists='replace', chunksize=600)
 
 def main():
     if len(sys.argv) == 4:
@@ -97,12 +97,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
